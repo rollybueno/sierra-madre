1ba6ced761cc4ad8c211a66e4381b9d854978fbc
Rolly Bueno <rollybueno@gmail.com>
2025-08-17 15:31:42 +0800

    Initial Version

---
+++ patterns/footer.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * Title: Footer
+ * Slug: sierra-journal/footer
+ * Category: sierra-journal, footer
+ */
+?>
+<!-- wp:group {"className":"has-white-color has-text-color is-footer","layout":{"type":"constrained","contentSize":"1600px"}} -->
+<div class="wp-block-group has-white-color has-text-color is-footer"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:site-title /-->
+
+<!-- wp:paragraph -->
+<p>Join our vibrant community today and stay informed about all the latest news, exciting updates, and exclusive offers that we have in store for you. </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>By becoming a member, you'll gain access to valuable information and opportunities that can enhance your experience with us. Don’t miss out on the chance to be a part of something special!</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Subscribe</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button">Join Us</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph -->
+<p>Quick Links</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Home Page</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>About Us</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Contact</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Blog Posts</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Support Center</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph -->
+<p>Resources</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Help Center</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>FAQs</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>User Guide</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Feedback</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Testimonials</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph -->
+<p>Follow Us</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:social-links -->
+<ul class="wp-block-social-links"><!-- wp:social-link {"url":"https://www.facebook.com","service":"facebook"} /-->
+
+<!-- wp:social-link {"url":"https://www.x.com","service":"x"} /-->
+
+<!-- wp:social-link {"url":"https://www.linkedin.com","service":"linkedin"} /-->
+
+<!-- wp:social-link {"url":"https://www.instagram.com","service":"instagram"} /-->
+
+<!-- wp:social-link {"url":"https://www.youtube.com","service":"youtube"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:separator -->
+<hr class="wp-block-separator has-alpha-channel-opacity"/>
+<!-- /wp:separator -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p>Crafted with care and coffee. © 2025 YourSite.com</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p>Privacy Policy</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Terms of Use</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Cookie Settings</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
---
+++ patterns/header.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Title: Header
+ * Slug: sierra-journal/header
+ * Category: sierra-journal, header
+ */
+?>
+<!-- wp:group {"layout":{"type":"constrained","contentSize":"1600px"}} -->
+<div class="wp-block-group"><!-- wp:group {"className":"header-bar","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group header-bar"><!-- wp:image {"width":"64px","height":"64px","className":"size-medium"} -->
+<figure class="wp-block-image is-resized size-medium"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/retro-brand.svg" alt="" style="width:64px;height:64px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:navigation {"ref":2003} /-->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Contact</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
