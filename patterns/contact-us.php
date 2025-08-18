eb3360b5d074919095a4e949d6b76d7fe095fb20
Rolly Bueno <rollybueno@gmail.com>
2025-08-18 11:42:49 +0800

    Pattern: Contact Us

---
+++ patterns/contact-us.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Title: Contact Us
+ * Slug: sierra-madre/contact-us
+ * Category: sierra-madre, contact-us
+ */
+?>
+<!-- wp:group {"layout":{"type":"constrained","contentSize":"1600px"}} -->
+<div class="wp-block-group"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"33.33%"} -->
+<div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:paragraph -->
+<p>Location</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading -->
+<h2 class="wp-block-heading">Contact Us</h2>
+<!-- /wp:heading -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p><strong>Email</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Reach us at:</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>info@company.com</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p><strong>Phone</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Call us at:</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>+1 (555) 123-4567</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p><strong>Office</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>123 Example Rd, Florida, USA</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button">Get Directions</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"66.66%"} -->
+<div class="wp-block-column" style="flex-basis:66.66%">
+    <!-- wp:image {"id":1852,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="http://localhost:10068/wp-content/uploads/2025/08/male-fitness-1024x683.jpg" alt="" class="wp-image-1852"/></figure>
+<!-- /wp:image -->
+</div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
